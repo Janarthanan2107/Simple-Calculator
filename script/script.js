469697e36cf7f0ba4ee9c5b3a7aba6988685a337
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-07-17 18:33:25 +0530

    A simple calculator with js

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// Get the input element
+var input = document.getElementById("input");
+
+// Get all the number buttons
+var numberButtons = document.querySelectorAll(".btn.number");
+
+// Get all the action buttons
+var actionButtons = document.querySelectorAll(".btn.action");
+
+// Get the equal button
+var equalButton = document.querySelector(".equalto-container");
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add click event listeners to number buttons
+    numberButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            input.value += button.innerText;
+        });
+    });
+
+    // Add click event listeners to action buttons
+    actionButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            input.value += " " + button.innerText + " ";
+        });
+    });
+
+    // Add click event listener to equal button
+    equalButton.addEventListener("click", function () {
+        var expression = input.value;
+        var result = eval(expression);
+        input.value = result;
+    });
+});
